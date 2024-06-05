eb17e85f3813ddb95c0c96b9169daff3d8f3f54c
Emiliano Tisato <emilianotisato@gmail.com>
2024-06-04 18:48:43 -0600

    Fix chunker behaviour that was leaving first item empty when larger that the config value

--- app/Services/DocumentManager.php
+++ app/Services/DocumentManager.php
@@ -1,38 +1,37 @@
-<?php 
+<?php
 
 namespace App\Services;
 
-use App\Models\Document;
 use App\Enums\DocumentType;
 use App\Jobs\ProcessPDFDocument;
 use App\Jobs\ProcessWebPageDocument;
+use App\Models\Document;
 use Illuminate\Support\Collection;
 
 class DocumentManager
 {
-	public function process(Document $document): void
-	{
-		if ($document->type === DocumentType::PDF) {
-			ProcessPDFDocument::dispatch($document);
-		} elseif ($document->type === DocumentType::WEB_PAGE) {
-			ProcessWebPageDocument::dispatch($document);
-		}
-	}
+    public function process(Document $document): void
+    {
+        if ($document->type === DocumentType::PDF) {
+            ProcessPDFDocument::dispatch($document);
+        } elseif ($document->type === DocumentType::WEB_PAGE) {
+            ProcessWebPageDocument::dispatch($document);
+        }
+    }
 
-	public function chunk(Document $document): Collection
-	{
-		$chunks = collect(explode("\n", $document->content))->reduce(function ($carry, $item) {
+    public function chunk(Document $document): Collection
+    {
+        $chunks = collect(explode("\n", $document->content))->reduce(function ($carry, $item) {
 			$last = count($carry) - 1;
-			if (strlen($carry[$last] . $item) <= config('services.chunker.size')) {
+			if (isset($carry[$last]) && strlen($carry[$last].$item) <= config('services.chunker.size')) {
 				$carry[$last] .= $item;
 			} else {
 				$carry[] = $item;
 			}
+		
 			return $carry;
-		}, 
-		[''] // initial value
-	);
-
+		}, []); // initial value is an empty array
+		
 		return collect($chunks);
-	}
+    }
 }
--- tests/Feature/Services/DocumentManagerTest.php
+++ tests/Feature/Services/DocumentManagerTest.php
@@ -50,6 +50,22 @@ class DocumentManagerTest extends TestCase
         $this->assertEquals('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', $chunks->first());
 
         $this->assertEquals('Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit assumenda nam illo accusamus laudantium pariatur ut doloremque ad, ex id aperiam. Optio quis saepe voluptatum consectetur tenetur nobis modi sit?', $chunks->last());
+    }
+
+    #[Test]
+    public function if_the_chunk_exceed_it_will_include_it_in_full_anyway()
+    {
+        $this->app->config->set('services.chunker', [
+            'size' => 500,
+        ]);
+        $document = Document::factory()->create([
+            'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit assumenda nam illo accusamus laudantium pariatur ut doloremque ad, ex id aperiam. Optio quis saepe voluptatum consectetur tenetur nobis modi sit?",
+        ]);
         
+        $chunks = app(DocumentManager::class)->chunk($document);
+        
+        $this->assertEquals(1, $chunks->count());
+
+        $this->assertEquals('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit assumenda nam illo accusamus laudantium pariatur ut doloremque ad, ex id aperiam. Optio quis saepe voluptatum consectetur tenetur nobis modi sit?', $chunks->first());
     }
 }
